2e1edabcd2c8d1a6f3e5757835ca91877c3f69a4
tbvjchvkfl / Developer <137769043+tbvjchvkfl@users.noreply.github.com>
2025-09-09 21:50:47 +0900

    액션 추가

---
+++ GunSlinger/Source/GunSlinger/Private/Controller/PlayerCharacterController.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "PlayerCharacterController.generated.h"
+
+struct FInputActionInstance;
+struct FInputActionValue;
+class UInputMappingContext;
+class UInputAction;
+class APlayerCharacter;
+
+UCLASS()
+class GUNSLINGER_API APlayerCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+
+public:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+	UPROPERTY(VisibleAnywhere, Category = "Property | State")
+	bool bIsAim = false;
+
+	UPROPERTY(VisibleAnywhere, Category = "Property | State")
+	bool bIsSwitchMovementState = false;
+
+	UPROPERTY(EditAnywhere, Category = "Property | Value")
+	float LookingRotationValue = 10.0f;
+
+	UPROPERTY(VisibleAnywhere, Category = "Property | Essential Data")
+	APlayerCharacter* OwnerCharacter;
+
+	UPROPERTY()
+	FTimerHandle AimTimerHandle;
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	APlayerCharacterController();
+
+private:
+	//==============================================================
+	//=                          Variable                          =
+	//==============================================================
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Value", meta = (AllowPrivateAccess = "true"))
+	float ZoomFactor = 0.0f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Value", meta = (AllowPrivateAccess = "true"))
+	float TimerInterupt = 0.01f;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultContext;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* LookAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* AimAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* BasicAttackAction;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Property | Input", meta = (AllowPrivateAccess = "true"))
+	UInputAction* SwitchMovementAction;
+
+	//==============================================================
+	//=                          Function                          =
+	//==============================================================
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+	virtual void SetupInputComponent()override;
+
+	void Move(const FInputActionInstance& Action);
+	void Look(const FInputActionInstance& Action);
+	void Aim(const FInputActionInstance& Action);
+	void UpdateAimState();
+	void EndAim(const FInputActionInstance& Action);
+	void BasicAttack();
+	void SwitchMovement(const FInputActionInstance& Action);
+};
